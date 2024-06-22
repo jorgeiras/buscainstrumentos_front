34e55d8e19a03da017391c6854de1a472ce17cfe
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-06-22 19:39:01 +0200

    base link fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jorgeiras.github.io/buscainstrumentos_front'
+  base: 'https://jorgeiras.github.io/buscainstrumentos_front/'
 })
